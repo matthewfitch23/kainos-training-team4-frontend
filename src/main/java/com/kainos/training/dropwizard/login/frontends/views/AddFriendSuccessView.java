388d315b5a794ea67c3e18171ceaa16b53895083
Matthew Fitch <m.fitch@kainos.com>
2015-09-04 13:31:06 +0100

    Add Java code to allow adding of friends

---
+++ src/main/java/com/kainos/training/dropwizard/login/frontends/views/AddFriendSuccessView.java
@@ -0,0 +1,9 @@
+package com.kainos.training.dropwizard.login.frontends.views;
+
+import io.dropwizard.views.View;
+
+public class AddFriendSuccessView extends View {
+	public AddFriendSuccessView(){
+		super("/add_friend_success.ftl");
+	}
+}
