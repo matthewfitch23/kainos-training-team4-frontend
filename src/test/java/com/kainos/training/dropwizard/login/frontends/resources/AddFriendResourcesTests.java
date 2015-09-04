f63f42dc49b73820f6d220c4c8301d7712e45f8f
James Taylor <j.taylor@kainos.com>
2015-09-04 14:31:58 +0100

    Create first test to add a friend successfully

--- src/test/java/com/kainos/training/dropwizard/login/frontends/resources/AddFriendResourcesTests.java
+++ src/test/java/com/kainos/training/dropwizard/login/frontends/resources/AddFriendResourcesTests.java
@@ -1,10 +1,15 @@
 package com.kainos.training.dropwizard.login.frontends.resources;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.*;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
+import org.eclipse.persistence.internal.oxm.schema.model.Any;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,15 +24,29 @@ public class AddFriendResourcesTests {
 	private ViewsResource resource;
 	private Person friend;
 	Response okResponse = Response.ok().build();
+	Response successResponse = Response.seeOther(
+			UriBuilder.fromUri("add-friend-success").build()).build();
 	
 	@Before
 	public void setup() {
 		mockedFriendClient = mock(FriendClient.class);
 		friend = new Person();
 		friend.setName(STANDARD_FRIEND_NAME);
-		when(mockedFriendClient.addFriend(friend)).thenReturn(okResponse);
+		// when(mockedFriendClient.addFriend(friend).thenReturn(successResponse));
+		
 				
 		resource = new ViewsResource(mockedFriendClient);
+	}
+	
+	@Test
+	public void testAddFriendCallsSuccessfully() {
+		when(mockedFriendClient.addFriend(any(Person.class))).thenReturn(okResponse);
+		Response addFriendResponse = resource.addFriend(friend.getName());
+		assertEquals(addFriendResponse.getStatus(), successResponse.getStatus());
+		assertEquals(addFriendResponse.getLocation(), successResponse.getLocation());
+		
+
+		verify(mockedFriendClient).addFriend(any(Person.class));
 	}
 	
 	@Test
